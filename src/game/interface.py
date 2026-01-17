e28d22f8500dda14ca385d96fa35f3cdaeecbf08
daoanhkhoa123 <158530319+daoanhkhoa123@users.noreply.github.com>
2026-01-17 17:50:19 +0700

    sth

--- src/game/interface.py
+++ src/game/interface.py
@@ -1,7 +1,14 @@
-from numpy.typing import NDArray
+from __future__ import annotations
 
-from src.game.client import Client
+from numpy.typing import NDArray
+from typing import TYPE_CHECKING
+
+# becuse of ciruclar import errors, and we actually just dont need the class
+# we just need its typing only
+if TYPE_CHECKING:
+    from src.game.client import Client
 
 
 class BaseInterface:
-    def show(self, client:Client, state:NDArray) -> None:    ...
+    def show(self, client: "Client", state: NDArray) -> None:
+        ...
