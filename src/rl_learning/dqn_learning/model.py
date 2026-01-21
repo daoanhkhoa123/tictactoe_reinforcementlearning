38437e8d2c66b3eb2ec8a3f5f44f39699f450dba
daoanhkhoa123 <158530319+daoanhkhoa123@users.noreply.github.com>
2026-01-21 08:08:12 +0700

    auto matic push

--- src/rl_learning/dqn_learning/model.py
+++ src/rl_learning/dqn_learning/model.py
@@ -1,4 +1,8 @@
 from torch import nn
+from src.common import index_to_coords
+from src.game.controller import BaseController
+from typing import Tuple
+import torch
 
 class DQN(nn.Module):
     def __init__(self, n_observations:int, n_actions:int, hidden:int = 128) -> None:
@@ -12,3 +16,13 @@ class DQN(nn.Module):
     def forward(self, x):
         return self.layers(x)
     
+class ModelController(BaseController[torch.Tensor, torch.Tensor]):
+    def __init__(self, model:nn.Module) -> None:
+        super().__init__()
+        self._model = model
+
+    def model_call(self, model_input: torch.Tensor) -> torch.Tensor:
+        return self._model(model_input)
+    
+    def post_processing(self, model_output: torch.Tensor) -> Tuple[int, int]:
+        return index_to_coords(int(model_output))
