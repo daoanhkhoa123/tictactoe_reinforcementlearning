c50d8846da09bffd3ff8c3f33b644001c82de853
daoanhkhoa123 <158530319+daoanhkhoa123@users.noreply.github.com>
2026-02-24 10:16:55 +0700

    sth

--- src/rl_learning/montelcarloo_treesearch/controller.py
+++ src/rl_learning/montelcarloo_treesearch/controller.py
@@ -12,14 +12,14 @@ from src.game.interface import BaseInterface
 from src.game.table import MarkType, Table
 from src.game.client import Client
 from dataclasses import dataclass
-from src.rl_learning.montelcarloo_treesearch.tree import Action, State, StateHashT, ActionNode, StateNode, MCTS
+from src.rl_learning.montelcarloo_treesearch.tree import Action, State, StateHashT, ActionNode, StateNode, MCTS, MCTSParams
 
 class MTCSController(BaseController[State, Action]):
     DEFAULT_INNER_STATE = MarkType.BLU
 
-    def __init__(self, mark_type: MarkType, utc_cons:float) -> None:
+    def __init__(self, mark_type: MarkType, mcts: MCTS) -> None:
         super().__init__()
-        self.tree = MCTS(utc_cons)
+        self.tree = mcts
         self.mark_type = mark_type
 
     def pre_processing(self, input_state: NDArray) -> State:
@@ -36,6 +36,7 @@ class MTCSController(BaseController[State, Action]):
     
 class MTCSCLient(Client):   ...
 
-def build_mtcslient(client:Client, utc_cons:float)-> MTCSCLient:
-    controller =  MTCSController(client.mark_type, utc_cons)
+def build_mtcslient(client:Client, params: Optional[MCTSParams] = None)-> MTCSCLient:
+    mtcs = MCTS(params or MCTSParams())
+    controller =  MTCSController(client.mark_type, mtcs)
     return MTCSCLient(client.name, client.mark_type, client.interface, controller)
